f062726d90d3abc93fcce099475ddd346578913c
Prathamesh Chougale <prathamesh17170@gmail.com>
2025-05-22 17:59:08 +0000

    d

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,6 +4,11 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add account association
+    "accountAssociation": {
+    "header": "eyJmaWQiOjEwODcwOTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhCQWMwODdFRWNlRTUxMkRlZTNkOTZDRmY5ZmJmM2M5NDIxQjllYWJFIn0",
+    "payload": "eyJkb21haW4iOiJtb25hZC1sb290LnZlcmNlbC5hcHAifQ",
+    "signature": "MHhjN2VkNGQwMzc3ODIyNTk3NDNlYTM4NDIwMDMxMDU5N2U1MjBhMmUxOWU3ODQ2MDJjMTlmYzEzMDE5ZjdmMjg5MDlhZDAxZjU3YmVkMzgwZGY4ODI0NzQ3MjBmZmNiZGE5N2JjNDg2ZWNmYjVhM2VkN2U2M2MzNDZmMDVhMTMyOTFj"
+  },
     frame: {
       version: "1",
       name: "Monad Farcaster MiniApp Template",
@@ -17,11 +22,6 @@ export async function GET() {
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: "#ffffff",
       webhookUrl: `${APP_URL}/api/webhook`,
-      "accountAssociation": {
-    "header": "eyJmaWQiOjEwODcwOTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhCQWMwODdFRWNlRTUxMkRlZTNkOTZDRmY5ZmJmM2M5NDIxQjllYWJFIn0",
-    "payload": "eyJkb21haW4iOiJtb25hZC1sb290LnZlcmNlbC5hcHAifQ",
-    "signature": "MHhjN2VkNGQwMzc3ODIyNTk3NDNlYTM4NDIwMDMxMDU5N2U1MjBhMmUxOWU3ODQ2MDJjMTlmYzEzMDE5ZjdmMjg5MDlhZDAxZjU3YmVkMzgwZGY4ODI0NzQ3MjBmZmNiZGE5N2JjNDg2ZWNmYjVhM2VkN2U2M2MzNDZmMDVhMTMyOTFj"
-  }
     },
   };
 
